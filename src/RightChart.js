e445ba01619977a9b887068494fac5f9bfbb0045
Srikanth Tekumudi <srikanthtekumudi11@gmail.com>
2021-03-26 01:23:04 +0530

    fiex the chart bug

--- src/RightChart.js
+++ src/RightChart.js
@@ -1,96 +1,70 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import ApexCharts from "apexcharts";
+import Chart from "react-apexcharts";
 import { useStateValue } from "./StateProvider";
 import "./RightChart.css";
 import moment from "moment";
 function RightChart() {
-  const [price_history, setprice_history] = useState([]);
-  const [name, setname] = useState([]);
-  const [data, setdata] = useState([]);
   const [{ currency }, dispatch] = useStateValue();
   const [startdate, setstartdate] = useState("2013-09-01");
   const [enddate, setenddate] = useState("2013-09-30");
+  const [param, setparam] = useState("");
+
+  console.log("param", param);
+
+  const getData = (list) => {
+    setparam({
+      options: {
+        chart: {
+          height: 280,
+          type: "area",
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        xaxis: {
+          type: "datetime",
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.9,
+            stops: [0, 90, 100],
+          },
+        },
+      },
+      series: [
+        {
+          data: list,
+        },
+      ],
+    });
+  };
   useEffect(() => {
     axios
       .get(
         `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}&start=${startdate}&end=${enddate}`
       )
       .then((res) => {
-        // console.log(res);
-        setprice_history(res.data.bpi);
-        // console.log(res.data.bpi);
-        // let x = createData(res.data.bpi);
-        let x = createData(res.data.bpi);
-        console.log(x, "hai");
-        setdata(x);
-        drawChart();
-        // console.log(price_history);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
+        let data = res.data.bpi;
+        console.log(data, "this data");
+        let list = [];
+        for (let i in data) {
+          list.push({
+            x: i,
+            y: data[i],
+          });
+        }
+        console.log(list);
+        getData(list);
       });
-
-    // console.log(data);
   }, [currency, startdate, enddate]);
 
-  //   useEffect(() => {
+  console.log("rerender");
 
-  //   }, [currency]);
-
-  useEffect(() => {
-    setdata(data);
-    drawChart();
-  }, [data]);
-
-  //   console.log(data, "data is wealth");
-  let createData = (data) => {
-    let chardata = [];
-    for (let i in data) {
-      chardata.push({
-        x: i,
-        y: data[i],
-      });
-    }
-
-    return chardata;
-  };
-
-  var options = {
-    chart: {
-      height: 280,
-      type: "area",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    series: [
-      {
-        data: data,
-      },
-    ],
-
-    xaxis: {
-      type: "datetime",
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shadeIntensity: 1,
-        opacityFrom: 0.7,
-        opacityTo: 0.9,
-        stops: [0, 90, 100],
-      },
-    },
-  };
-
-  let drawChart = () => {
-    let chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
-  };
-
-  let getData = (e) => {
+  let handleData = (e) => {
     console.log(e);
     e.preventDefault();
     let start = e.target.start.value;
@@ -105,12 +79,19 @@ function RightChart() {
 
   return (
     <div className="rightchart">
-      <form onSubmit={getData} className="rightchart__form">
+      <form onSubmit={handleData} className="rightchart__form">
         <input name="start" type="date"></input>
         <input name="end" type="date"></input>
         <button type="submit">Get Data</button>
       </form>
-      <div className="chart" id="chart"></div>
+      {Object.keys(param).length > 0 && (
+        <Chart
+          options={param.options}
+          series={param.series}
+          type="area"
+          width="500"
+        />
+      )}
     </div>
   );
 }
